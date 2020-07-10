e3081c0e498676e57520b9ac678cb02b057a4af1
‘maximeaperez92’ <maxime.aperez92@gmail.com>
2020-07-10 08:28:30 +0200

    update

---
+++ Assets/Scripts/Accessor/TAccessor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Accessor
+{
+    public class TAccessor<T>
+    {
+        private static TAccessor<T> _instance;
+        private List<T> _modules;
+
+        public static TAccessor<T> Instantiate()
+        {
+            if (_instance == null) _instance = new TAccessor<T>();
+            
+            return _instance;
+        }
+
+        private TAccessor()
+        {
+            _modules = new List<T>();
+        }
+
+        public List<T> GetModules()
+        {
+            return _modules;
+        }
+
+        public void AddModule(T module)
+        {
+            _modules.Add(module);
+        }
+    }
+}
---
+++ Assets/Scripts/Module/FollowTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+    
+    void Start()
+    {
+        Accessor.TAccessor<FollowTarget>.Instantiate().AddModule(this);
+    }
+}
